960e37ae0df9147e85807f5d41c11a9e6b23fe98
joonsooan <joonsooan@gmail.com>
2024-07-10 11:55:27 +0900

    Fixed screen size and made GridManager singleton

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,15 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
+    public static GridManager Instance;
+
     [Header("Tile Settings")]
     [SerializeField] int width = 6;
     [SerializeField] int height = 5;
     [SerializeField] float tileSpacing = 1.2f; // 타일 간격
     [SerializeField] GameObject tilePrefab; // Tile Prefab
     [SerializeField] Color tileColor = Color.green; // 타일 색상 설정
+
+    [Header("Grid Offset")]
+    [SerializeField] float xOffset = -8;
+    [SerializeField] float yOffset = -1;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void Start()
     {
@@ -22,7 +39,7 @@ public class GridManager : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
-                Vector3 position = new Vector3(x * tileSpacing, y * tileSpacing, 0);
+                Vector3 position = new Vector3(xOffset + x * tileSpacing, yOffset + y * tileSpacing, 0);
                 GameObject tile = Instantiate(tilePrefab, position, Quaternion.identity, transform);
 
                 // 타일의 색상을 설정합니다.
